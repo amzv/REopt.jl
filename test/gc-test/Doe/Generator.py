bb7030a95223268209536ec7a0826989e4d6ef40
Zarate Villazon, Angel Mario <amzv3@gatech.edu>
2023-02-28 12:21:20 -0500

    big updates for test case

--- test/gc-test/Doe/Generator.py
+++ test/gc-test/Doe/Generator.py
@@ -5,65 +5,70 @@ import json
 import pandas as pd
 
 
-df = pd.read_csv('test\gc-test\Doe\DoE.csv')
+df = pd.read_csv('test/gc-test/Doe/test1.csv')
 print(df.head)
 
-path = "test\gc-test\Doe\DoE.xlsx" # "path_to_csv": str(row[17]),
+path = "test/gc-test/Doe/test_load.csv" # "path_to_csv": str(row[17]),
 place = "56f071345457a351557112bc" #https://apps.openei.org/USURDB/rate/view/56f071345457a351557112bc
 
 for index, row in df.iterrows():
 
     item_data = {}
 
+    if str(row[21]) == 'true':
+        grid=True
+    else:
+        grid=False
+
     item_data["Site"] = {
-        "longitude": str(row[0]),
-        "latitude": str(row[1]),
-        "roof_squarefeet": str(row[2]),
-        "land_acres": str(row[3]),
-        "node": str(row[4])
+        "longitude": row[0],
+        "latitude": row[1],
+        "roof_squarefeet": row[2],
+        "land_acres": row[3],
+        "node": row[4]
     }
 
     item_data["PV"] = {
-        "macrs_bonus_fraction": str(row[5]), #Find out meaning
-        "installed_cost_per_kw": str(row[6]),
-        "tilt": str(row[7]),
-        "degradation_fraction": str(row[8]),
-        "macrs_option_years": str(row[9]),
-        "federal_itc_fraction": str(row[10]),
-        "module_type": str(row[11]), #standar : 0, premium : 1 , thin_film: 2
-        "array_type": str(row[12]), #fixed : 1, one_axis : 2, one_axis_one_track : 3
-        "om_cost_per_kw": str(row[13]),
-        "macrs_itc_reduction": str(row[14]),
-        "azimuth": str(row[15]),
-        "federal_rebate_per_kw": str(row[16])
+        "macrs_bonus_fraction": row[5], #Find out meaning
+        "installed_cost_per_kw": row[6],
+        "tilt": row[7],
+        "degradation_fraction": row[8],
+        "macrs_option_years": row[9],
+        "federal_itc_fraction": row[10],
+        "module_type": row[11], #standar : 0, premium : 1 , thin_film: 2
+        "array_type": row[12], #fixed : 1, one_axis : 2, one_axis_one_track : 3
+        "om_cost_per_kw": row[13],
+        "macrs_itc_reduction": row[14],
+        "azimuth": row[15],
+        "federal_rebate_per_kw": row[16]
     }
     item_data["ElectricLoad"] = {
         "path_to_csv": path,
-        "critical_load_fraction": str(row[17]),
-        "year": str(row[18])
+        "critical_load_fraction": row[18],
+        "year": str(row[17])
     },
     item_data["ElectricStorage"] =  {
-        "total_rebate_per_kw": str(row[19]),
-        "macrs_option_years": str(row[20]),
-        "can_grid_charge": str(row[21]),
-        "macrs_bonus_fraction": str(row[22]),
-        "replace_cost_per_kw": str(row[23]),
-        "replace_cost_per_kwh": str(row[24]),
-        "installed_cost_per_kw": str(row[25]),
-        "installed_cost_per_kwh": str(row[26]),
-        "total_itc_fraction": str(row[27]),
-        "charge_efficiency": str(row[28]) #origianl ref is 0.975**(1/2)*0.96
+        "total_rebate_per_kw": row[19],
+        "macrs_option_years": row[20],
+        "can_grid_charge": grid,
+        "macrs_bonus_fraction": row[22],
+        "replace_cost_per_kw": row[23],
+        "replace_cost_per_kwh": row[24],
+        "installed_cost_per_kw": row[25],
+        "installed_cost_per_kwh": row[26],
+        "total_itc_fraction": row[27],
+        "charge_efficiency": row[28] #origianl ref is 0.975**(1/2)*0.96
     },
     item_data["ElectricTariff"] = {
-        "urdb_label": place #updated value for princeton
+        "urdb_label": str(place) #updated value for princeton
     },
     item_data["Financial"] =  {
-        "elec_cost_escalation_rate_fraction": str(row[29]),
-        "offtaker_discount_rate_fraction": str(row[30]),
-        "owner_discount_rate_fraction": str(row[31]),
-        "offtaker_tax_rate_fraction": str(row[32]),
-        "owner_tax_rate_fraction": str(row[33]),
-        "om_cost_escalation_rate_fraction": str(row[34])
+        "elec_cost_escalation_rate_fraction": row[29],
+        "offtaker_discount_rate_fraction": row[30],
+        "owner_discount_rate_fraction": row[31],
+        "offtaker_tax_rate_fraction": row[32],
+        "owner_tax_rate_fraction": row[33],
+        "om_cost_escalation_rate_fraction": row[34]
     }
 
     # temp.append(item_data)
